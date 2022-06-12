994ecd43a2b2d1e3cfe5f6693a8ea3a398336460
Eric Loc <eloc1385@gmail.com>
2022-06-11 18:37:31 -0700

    mark task done and not done check mark, done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tasks;
 
+// Function to load tasks from local storage
 const savedTasks = JSON.parse(localStorage.getItem('tasks'));
 if (Array.isArray(savedTasks)) {
     tasks = savedTasks;
@@ -20,6 +21,7 @@ else {
 
 renderTask();
 
+// Function to create task
 function createTask(title) {
     const id = '' + new Date().getTime();
     tasks.push({
@@ -30,6 +32,7 @@ function createTask(title) {
     saveTasks();
 }
 
+// Function to remove task
 function removeTask(idToDelete) {
     tasks = tasks.filter( function (tasks) {
         if(tasks.id === idToDelete) {
@@ -43,18 +46,17 @@ function removeTask(idToDelete) {
     saveTasks();
 }
 
-
-function completeTask() {
-    var checkMark = document.getElementById('unchecked');
-    checkMark.addEventListener("click", function(event){
-        if(event.target.id === 'unchecked'){
-            event.target.id = 'checked';
-        }
-    }, false);
-}
+// Function to mark task done and undone
+var checkMark = document.querySelector('ul');
+checkMark.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+}, false);
 
 
 
+// Function to save tasks to local storage
 function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
@@ -86,16 +88,11 @@ function renderTask() {
     document.getElementById('task-list').innerHTML = '';
 
     tasks.forEach(function(tasks) {
-        const element = document.createElement('div');
-        element.classList.add('task-item');
-        element.setAttribute('id', 'unchecked');
-        element.setAttribute("onclick", "completeTask()");
+        const element = document.createElement('li');
         element.innerText = tasks.title;
-
         const delButton = document.createElement('button');
         delButton.classList.add('delete-button');
         delButton.innerText = 'X';
-        delButton.style = 'margin-left: 12px;';
         delButton.onclick = deleteTask;
         delButton.id = tasks.id;
         element.appendChild(delButton);
